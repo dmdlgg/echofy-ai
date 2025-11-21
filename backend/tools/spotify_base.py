406df4d0b3199a77ccec04862614929cffb289b9
eduardo <dumedolago@gmail.com>
2025-11-20 22:40:10 -0300

    ajuste e documentação da função utilizada para obter tokens

--- backend/tools/spotify_base.py
+++ backend/tools/spotify_base.py
@@ -2,11 +2,13 @@ import requests
 from dotenv import load_dotenv
 import os
 
+
 load_dotenv()
 
+# essa função server para obter o bearer token para ter acesso a api do spotify
 def get_spotify_token():
 
-    b64_str = os.getenv("B64_STR")
+    b64_str = os.getenv("B64_STR") # codigo em base64 que é utilizado para gerar o token
 
     headers = {
         "Authorization": f"Basic {b64_str}",
@@ -24,6 +26,3 @@ def get_spotify_token():
     )
 
     return response.json().get("access_token")
-
-token = get_spotify_token()
-print(token)
